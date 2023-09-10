ef9775294e007d6a127141bbc5554c04d156ed18
miiihaaas <miiihaaas@gmail.com>
2023-09-10 09:49:50 +0200

    0.41 mejl

--- onetouch/main/routes.py
+++ onetouch/main/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import current_user
+from flask_mail import Message, Mail
+from onetouch import mail
 
 
 main = Blueprint('main', __name__)
@@ -22,7 +24,20 @@ def about():
     return render_template('about.html', title='O softveru')
 
 
-@main.route('/ajax', methods=['POST'])
-def ajax():
-    new_value = request.form.get('new_value')
-    return new_value
+@main.route('/send_mail', methods=['POST', 'GET'])
+def send_mail():
+    sender_email = 'noreply@uplatnice.online'
+    recipient_email = 'miiihaaas@gmail.com'
+    subject = 'Test slanja mejla klikom na link iz aplikacije'
+    body = 'Ovo je poruka koja se salje klikom na link iz aplikacije'
+    
+    message = Message(subject, sender=sender_email, recipients=[recipient_email])
+    message.body = body
+    
+    try:
+        mail.send(message)
+        flash('Mejl je poslat', 'success')
+        return redirect(url_for('main.home'))
+    except Exception as e:
+        flash('Greska prilikom slanja mejla: ' + str(e), 'danger')
+        return redirect(url_for('main.home'))
