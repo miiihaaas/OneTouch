56169bea5686e00ea826fb47b10d4958bd96d8bc
miiihaaas <miiihaaas@gmail.com>
2023-09-14 20:58:20 +0200

    0.62 reset pass #2

--- onetouch/models.py
+++ onetouch/models.py
@@ -18,6 +18,17 @@ class User(db.Model, UserMixin): #! ovo je samo administrator škole
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"{self.id}, '{self.name} {self.surname}'"
 
 
 class School(db.Model):
