5cca96349303ae617b9d2ca2b0fe2d4de46b4173
miiihaaas <miiihaaas@gmail.com>
2024-04-12 21:05:17 +0200

    ponovo

--- onetouch/transactions/routes.py
+++ onetouch/transactions/routes.py
@@ -471,6 +471,9 @@ def debt_archive(debt_id):
     single = False
     send = False
     file_name = uplatnice_gen(records, purpose_of_payment, school_info, school, single, send)
+    if not file_name:
+        flash('Doslo je do greške prilikom generisanja uplatnice. Proverite da li su svi podaci na uplatnici ispravno popunjeni ili kontaktirajte našu podršku.', 'danger')
+        return redirect(url_for('transactions.debt_archive', debt_id=debt_id))
     
     gen_dept_report(records)
 
@@ -495,6 +498,9 @@ def send_payment_slips(debt_id):
     single = True
     send = True
     file_name = uplatnice_gen(records, purpose_of_payment, school_info, school, single, send)
+    if not file_name:
+        flash('Doslo je do greške prilikom generisanja uplatnica. Proverite da li su svi podaci na uplatnici ispravno popunjeni ili kontaktirajte našu podršku.', 'danger')
+        return redirect(url_for('transactions.debt_archive', debt_id=debt_id))
     flash('Uspešno ste poslali mejlove roditeljima.', 'success')
     return redirect(url_for('transactions.debt_archive', debt_id=debt_id))
 
@@ -512,6 +518,9 @@ def send_single_payment_slip(record_id):
     single = True
     send = True
     file_name = uplatnice_gen(records, purpose_of_payment, school_info, school, single, send)
+    if not file_name:
+        print('Doslo je do greške prilikom generisanja uplatnice. Proverite da li su svi podaci na uplatnici ispravno popunjeni ili kontaktirajte našu podršku.')
+        return redirect(url_for('transactions.send_single_payment_slip', record_id=record_id))
     flash('Uspešno ste poslali mejl roditelju.', 'success')
     return redirect(url_for('transactions.debt_archive', debt_id=debt_id))
 
