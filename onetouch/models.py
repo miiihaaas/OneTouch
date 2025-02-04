74e91bc325219eeacb5aa3453fbf9a674224ef84
miiihaaas <miiihaaas@gmail.com>
2025-02-04 19:58:40 +0100

    bug fixed: greška pri slanju mejla v0.2

--- onetouch/models.py
+++ onetouch/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin): #! ovo je samo administrator škole
     #     s = Serializer(app.config['SECRET_KEY'], expires_sec)
     #     return s.dumps({'user_id': self.id}).decode('utf-8')
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], salt='reset-key')
+        s = Serializer(app.config['SECRET_KEY'], expires_sec=expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
     # @staticmethod
     # def verify_reset_token(token):
